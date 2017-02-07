2470a333bb5f88ae4440693913fdaad5e1c2105e
Jonas Auer <jonas.auer.94@gmail.com>
2017-02-07 21:50:57 +0100

    Add a working implementation for the local server

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
 'use strict'
 
-const net = require('net')
-const readline = require('readline')
-
-const server = net.createServer(socket => {
-  console.log('! client connected')
-  const connection = readline.createInterface({
-    input: socket
-  })
-  const cli = readline.createInterface({
-    input: process.stdin
-  })
-  connection.on('line', line => {
-    console.log(`> ${line}`)
-  })
-  cli.on('line', line => {
-    socket.write(`${line}\n`)
-  })
-})
-server.listen(3007, 'localhost')
+const localServer = require('./lib/local-server')
