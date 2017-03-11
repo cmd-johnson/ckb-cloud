d9338838a7929eb89dbe099117fe2be4223c39b2
Jonas Auer <jonas.auer.94@gmail.com>
2017-03-11 19:22:35 +0100

    Remove commented-out code

--- index.js
+++ index.js
@@ -18,37 +18,3 @@ localServer.connect(LOCAL_SOCKET)
   console.log(`REST-server listening on port ${port}`)
 })
 .catch(console.error)
-
-/*
-const localServer = require('./lib/local-server')
-
-const express = require('express')
-const bodyParser = require('body-parser')
-
-const PORT = process.env.PORT || 3007
-
-const app = express()
-
-app.use(bodyParser.json({
-  limit: '1kb'
-}))
-
-app.get('/keys', (req, res, next) => {
-  localServer.broadcastCommand({
-    command: 'list_keys'
-  })[0]
-  .then(answer => { res.status(200); res.send(answer.keys.sort()) })
-  .catch(err => next(JSON.stringify(err)))
-})
-app.post('/keys/:key', (req, res, next) => {
-  let command = req.body
-  command.key = req.params.key
-  localServer.broadcastCommand(command)[0]
-  .then(answer => { res.status(200); res.end() })
-  .catch(err => next(JSON.stringify(err)))
-})
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
-})
-*/
